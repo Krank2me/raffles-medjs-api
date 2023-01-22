e5abd5987dbe44b36cd2a6434ff7643c20e495cb
Frank Betancur <alejok6@gmail.com>
2023-01-21 20:51:11 -0500

    feature: get all raffle competitors

--- api/raffles/index.js
+++ api/raffles/index.js
@@ -1,7 +1,10 @@
 const { Router } = require("express");
 const addCompetitor = require("./raffle.controller.js");
+const getWiners = require("./winers.controllers.js");
 
 const router = Router();
+
 router.post("/addCompetitor", addCompetitor);
+router.get("/getWiners", getWiners);
 
 module.exports = router;
---
+++ api/raffles/winers.controllers.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const Airtable = require("airtable");
+const { API_KEY, BASE } = process.env;
+const base = new Airtable({ apiKey: API_KEY }).base(BASE);
+
+async function getWiners(req, res) {
+  persons = [];
+  try {
+    base('competitor').select({
+      maxRecords: 500,
+      view: "Grid view"
+  }).eachPage(function page(records, fetchNextPage) {
+      records.forEach(function(record) {
+          persons.push(record.get('fullname'));
+      });
+      res.status(200).send({ data: persons });
+      fetchNextPage();
+  }, function done(err) {
+      if (err) { console.error(err); return; }
+  });
+  } catch (error) {
+    res.status(500).send({ erroMessage: "server internal error" });
+  }
+}
+
+module.exports = getWiners;
